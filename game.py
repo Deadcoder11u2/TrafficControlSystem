c058ca1cd7a2870e400647e0938c1b320f52d6fb
Srikanth Iyengar <ksrikanth3012@gmail.com>
2022-04-16 12:01:45 +0530

    Drawing roads

--- game.py
+++ game.py
@@ -1,4 +1,9 @@
 import pygame 
+from pygame import gfxdraw
+
+#Colors 
+RED = (255, 0, 0)
+
 
 # Initialize the game
 pygame.init()
@@ -9,12 +14,28 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Space Invaders")
 
 #player
-car = pygame.image.load('car_4.png')
-carX = 370
-carY = 480
+car = pygame.image.load('car_1.png')
+carX = 300
+carY = 300
 
 def carPlace():
     screen.blit(car, (carX, carY))
+
+def draw_circle(x, y, color):
+    pygame.draw.circle(screen, color, (x, y), 5, 5)
+
+def draw_road():
+    road = [
+        [(0, 150), (150, 150)],
+        [(0, 250), (250, 250)],
+        [(0, 400), (250, 400)],
+        [(0, 500), (250, 500)],
+        [(250, 250), (250, 400)],
+        [(150, 0), (150, 150)],
+        [(250, 0), (250, 150)]
+    ]
+    for line in road:
+        pygame.draw.line(screen, RED, line[0], line[1])
 
 running = True 
 while running:
@@ -22,5 +43,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             break
-    carPlace()
+    # draw_circle(0, 0, (255, 0, 0))
+    draw_road()
     pygame.display.update()
